619ceb13349036ccf844c5bf2b8a05fed5148219
ZaL <z4ls@live.com>
2016-05-03 19:44:26 +0300

    add bundle

--- src/constants/config.js
+++ src/constants/config.js
@@ -1,5 +1,5 @@
 'use strict';
-//export const API_ROOT = 'http://mushtryati.ideasowners.net/api/v1';
+export const API_ROOT = 'http://mushtryati.ideasowners.net/api/v1';
 //export const API_ROOT = 'http://instachat.izal.me/api/v1';
-export const API_ROOT = 'http://mushtryati.app/api/v1';
+//export const API_ROOT = 'http://mushtryati.app/api/v1';
 export const API_TOKEN = 'API_TOKEN';
