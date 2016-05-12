b65207cfd29559ade0143cb64a52f996ca3b371b
ZaL <z4ls@live.com>
2016-05-12 18:31:51 +0300

    added new bundle

--- src/Root.js
+++ src/Root.js
@@ -3,19 +3,14 @@ import { AppState } from 'react-native';
 import { Provider } from 'react-redux';
 import configureStore from './reducers/configure-store';
 import App from './App';
-import codePush from "react-native-code-push";
+import CodePush from "react-native-code-push";
 const store = configureStore();
 
 export default class Root extends Component {
 
   componentDidMount() {
     console.disableYellowBox = true;
-
-    AppState.addEventListener("change", (newState) => {
-      if(newState === "active") {
-      codePush.sync({installMode: codePush.InstallMode.ON_NEXT_RESUME});
-      }
-    });
+    CodePush.sync();
   }
 
   render() {
