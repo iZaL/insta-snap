31c7428900c1bb8a2703dfa427ba612c17cc28e2
ZaL <z4ls@live.com>
2017-01-23 17:13:05 +0300

    update api endpoint

--- src/constants/config.js
+++ src/constants/config.js
@@ -1,5 +1,3 @@
 'use strict';
-// export const API_ROOT = 'http://mushtryati.ideasowners.net/api/v1';
-//export const API_ROOT = 'http://instachat.izal.me/api/v1';
-export const API_ROOT = 'http://instasnap.dev/api/v1';
+export const API_ROOT = 'http://instasnap.izal.me/api/v1';
 export const API_TOKEN = 'API_TOKEN';
