fca9c36fec3818e0de4e0bc14c1f7ec9c8fa8b40
ZaL <z4ls@live.com>
2017-02-11 07:58:42 +0300

    fix queries

--- index.ios.js
+++ index.ios.js
@@ -2,10 +2,10 @@ import React from 'react';
 import { AppRegistry } from 'react-native';
 import Root from './src/Root';
 //delete GLOBAL.XMLHttpRequest;
-const _XHR = GLOBAL.originalXMLHttpRequest ?
-  GLOBAL.originalXMLHttpRequest :
-  GLOBAL.XMLHttpRequest;
-
-XMLHttpRequest = _XHR;
-console.disableYellowBox= true;
+// const _XHR = GLOBAL.originalXMLHttpRequest ?
+//   GLOBAL.originalXMLHttpRequest :
+//   GLOBAL.XMLHttpRequest;
+//
+// XMLHttpRequest = _XHR;
+// console.disableYellowBox= true;
 AppRegistry.registerComponent('insta', () => Root);
